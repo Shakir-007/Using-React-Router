e4365d83559a1fcc69c21db03d344623812e8825
Shakir-007 <56887751+Shakir-007@users.noreply.github.com>
2025-04-14 14:20:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
